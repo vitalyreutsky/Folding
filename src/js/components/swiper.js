1a18b5019bc7541220bb38d5b40debd7d9953d22
Vitaly Reutsky <vreutsky77@mail.ru>
2023-02-06 11:56:21 +0300

    Сверстал секцию facts

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -3,8 +3,9 @@ import Swiper, {
   Keyboard,
   Navigation,
   Pagination,
-  Scrollbar,
   Thumbs,
+  Scrollbar,
+  EffectCoverflow,
 } from "swiper";
 
 //!hero-swiper
@@ -28,6 +29,54 @@ const swiper = new Swiper(".hero-swiper", {
       return `
          <span class="hero-swiper__current">0<span class="${currentClass}"></span></span><span class="hero-swiper__total">0<span class="${totalClass}"></span></span>
       `;
+    },
+  },
+});
+
+//!facts
+Swiper.use([
+  Navigation,
+  Pagination,
+  Keyboard,
+  Thumbs,
+  Scrollbar,
+  EffectCoverflow,
+]);
+const factsSwiper = new Swiper(".facts-swiper", {
+  slidesPerGroup: 1,
+  slidesPerView: 3,
+  loop: true,
+  centeredSlides: true,
+  speed: 1000,
+  effect: "coverflow",
+
+  coverflowEffect: {
+    rotate: 50,
+    stretch: -60,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+    scale: 0.8,
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  navigation: {
+    nextEl: ".facts__btn-next",
+    prevEl: ".facts__btn-prev",
+  },
+
+  thumbs: {
+    swiper: {
+      el: ".facts-swiper__mini",
+      slidesPerView: 6,
+      spaceBetween: 30,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
     },
   },
 });
